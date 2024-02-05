ba49b325bacb3ffefc98b6cc7724866046bdf147
marvinhoguet <marvlouna@gmail.com>
2024-02-05 13:50:30 +0100

    changement package.json

--- mflix/pages/_app.js
+++ mflix/pages/_app.js
@@ -1,11 +1,16 @@
 import './output.css';
 import './styles.css';
 import '../components/activity.css';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
 
-    return <Component {...pageProps} />;
+    return (
+        <div>
+        <SpeedInsights id="speed-insights" />
+        <Component {...pageProps} />
+        </div>
+    )
 }
 
 export default MyApp;
