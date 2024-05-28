6054b03dc50edb1e15469a61c315b30d0355b6c6
marvinhoguet <marvlouna@gmail.com>
2024-05-28 20:33:51 +0200

    feat(): modification page success.tsx

--- mflix/components/searchbar.tsx
+++ mflix/components/searchbar.tsx
@@ -15,34 +15,42 @@ const SearchBarWithVoyagesList: React.FC<{ voyages: any[] }> = ({ voyages }) => 
     );
 
     return (
-        <div>
-            <div className="flex justify-center items-center mt-20">
-                <input
-                    type="text"
-                    placeholder="Rechercher un voyage"
-                    className="px-4 py-2 border  rounded-lg focus:outline-none focus:ring focus:border-blue-300 w-96 h-12 text-black"
-                    value={searchQuery}
-                    onChange={(e) => handleSearch(e.target.value)}
+        <div className="relative">
+            <div className="absolute inset-0 z-0 ">
+                <img
+                    src="Fichier21.svg"
+                    alt="nuage"
+                    className="blur-md"
+                    style={{ width: '300%', height: '300%', left: 0, top: 0 }} // Position à gauche et en haut
                 />
             </div>
-            {showVoyages && ( // Afficher les voyages seulement si showVoyages est vrai
+            <div className="z-10 relative">
                 <div className="flex justify-center items-center mt-20">
-                    <div className="grid grid-cols-3 gap-4">
-                        {filteredVoyages.map((voyage) => (
-                            <div key={voyage.id} className="border border-gray-300 rounded-lg p-4">
-                                <Link href={`/voyage/${voyage.id}`}>
-
+                    <input
+                        type="text"
+                        placeholder="Rechercher un voyage"
+                        className="px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300 w-96 h-12 text-black"
+                        value={searchQuery}
+                        onChange={(e) => handleSearch(e.target.value)}
+                    />
+                </div>
+                {showVoyages && (
+                    <div className="flex justify-center items-center mt-20">
+                        <div className="grid grid-cols-3 gap-4">
+                            {filteredVoyages.map((voyage) => (
+                                <div key={voyage.id} className="border border-gray-300 rounded-lg p-4">
+                                    <Link href={`/voyage/${voyage.id}`}>
                                         <img src={voyage.image} alt={voyage.name} className="rounded-lg"/>
                                         <h3 className="text-xl font-bold">{voyage.name}</h3>
                                         <p>{voyage.content}</p>
                                         <p className="text-blue-600 font-bold">{voyage.price} €</p>
-
-                                </Link>
-                            </div>
-                        ))}
+                                    </Link>
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 };
--- mflix/pages/success/success.tsx
+++ mflix/pages/success/success.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const Success = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            router.push('/').then(r => r);
+        }, 3000);
+
+        // Clean up the timer when the component is unmounted
+        return () => clearTimeout(timer);
+    }, [router]);
+
     return (
         <div className="container mx-auto">
             <div className="flex justify-center items-center h-screen">
                 <div className="text-center">
                     <h2 className="text-4xl font-bold text-blue-900">Merci pour votre achat !</h2>
-                    <p className="mt-2 text-blue-900">Vous recevrez un email de confirmation dans quelques minutes.</p>
+                    <p className="mt-2 text-blue-900">Vous allez etre redirigé vers la page d'acceuil.</p>
                 </div>
             </div>
         </div>
     );
-
-}
+};
 
 export default Success;
 
