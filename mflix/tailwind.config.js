bffa8a528fe76b5853f2185855135e43a2fd10ea
marvinhoguet <marvlouna@gmail.com>
2024-05-30 20:26:41 +0200

    feat(): ajout de commentaire

---
+++ mflix/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+/** test */
+module.exports = {
+    content: [
+        "./pages/**/*.{ts,tsx}",
+        "./public/**/*.html",
+        "./components/**/*.{ts,tsx}",
+    ],
+    plugins: [
+        require("flowbite/plugin")
+    ],
+    theme: {
+        fontFamily: {
+            'sans': ['Tahoma', 'Arial', 'sans-serif'],
+            'serif': ['Tahoma', 'serif'],
+            'mono': ['Tahoma', 'monospace'],
+        },
+    },
+};
