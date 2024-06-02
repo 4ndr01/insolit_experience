6277ab39c093c933302f92b5ce0d345c1cfebddd
marvinhoguet <marvlouna@gmail.com>
2024-06-02 22:21:06 +0200

    feat(): ajout hover sur les voyages

--- mflix/tailwind.config.js
+++ mflix/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 /** test */
 module.exports = {
+    safelist: [
+        {
+            pattern: /./, // Conserver toutes les classes utilitaires
+            variants: ['hover', 'focus'], // Conserver les variantes hover et focus
+        },
+    ],
     content: [
         "./pages/**/*.{ts,tsx}",
         "./public/**/*.html",
