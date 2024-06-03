a599da2175d035bc788d6d0203394a48b73f22cf
marvinhoguet <marvlouna@gmail.com>
2024-06-03 10:56:57 +0200

    feat():création des activités

---
+++ mflix/components/activity/list.tsx
@@ -0,0 +1,116 @@
+
+const Activity = [
+    {
+        id: 7,
+        name: "Roulotte en bois",
+        position: "Nantes",
+        content:
+            "Évadez-vous du quotidien en séjournant dans notre charmante roulotte en bois. Située en pleine nature, cette roulotte vous offre une expérience unique et authentique, combinant le charme d’un habitat nomade avec tout le confort moderne. Parfait pour une escapade romantique ou une aventure familiale, notre roulotte en bois est l’option idéale pour ceux qui recherchent un séjour insolite.\n",
+        image: "/Roulotteenbois.jpg",
+        image2: "/4image.svg",
+        price: 1500,
+        repas: "sur place",
+        nbSalleDeBain: 2,
+        wifi: "gratuit",
+        nbPersonne: 2,
+        category: "Culture",
+        avis: "Emma D.: \"Une escapade parfaite dans un cadre enchanteur. La roulotte en bois est adorable et bien équipée.\"\n",
+        avis2: "Marc L.: \"Un séjour magique en pleine nature. Les enfants ont adoré l’aventure !\"\n"
+
+    },
+    {
+        id: 8,
+        name: "Parapente",
+        position: "Toulouse",
+        content:
+            "Défiez les cieux et ressentez l'excitation de voler librement au-dessus des montagnes avec notre expérience de parapente en montagne. \n" +
+            "Envolez-vous vers de nouvelles sensations, avec des vues panoramiques à couper le souffle et une dose d'adrénaline inoubliable. \n" +
+            "Prêt pour l'aventure ? Cliquez maintenant pour réserver votre vol et laissez-vous emporter vers les hauteurs  !\n",
+        image: "/Parapente.jpg",
+        image2: "/4image.svg",
+        price:  85,
+        repas: "sur place",
+        nbSalleDeBain: 1,
+        wifi: "gratuit",
+        nbPersonne: 3,
+        category: "Culture"
+
+    },
+    {
+        id: 9,
+        name: "Les catacombes de Paris",
+        position: "Paris",
+        content:
+            "Découvrez les Catacombes de Paris, un lieu fascinant et mystérieux sous la capitale. Explorez ces tunnels historiques remplis d'ossements et apprenez-en plus sur l'histoire de Paris. Une visite unique qui vous plongera dans les profondeurs de la ville et de son passé.\n" +
+            "\n",
+        image: "/LescatacombesdeParis.jpg",
+        image2: "/4image.svg",
+        price: 29,
+        repas: "sur place",
+        nbSalleDeBain: 3,
+        wifi: "gratuit",
+        nbPersonne: 4,
+        category: "Relaxation",
+        avis: "Nathan W : \"Visite impressionnante des Catacombes de Paris. L'endroit est fascinant et un peu effrayant, parfait pour les amateurs d'histoire. Je recommande vivement !\"\n",
+        avis2: "Myrielle M. : \"Super expérience ! Les Catacombes de Paris sont vraiment uniques. On apprend beaucoup sur l'histoire de la ville. À ne pas manquer si vous êtes à Paris.\"\n"
+
+    },
+    {
+        id: 10,
+        name: "Le Colorado Provençal ",
+        position: "Marseille",
+        content:
+            "Description : \n" +
+            "Découvrez le Colorado Provençal, un site naturel exceptionnel situé en plein cœur de la Provence. Ses roches aux couleurs flamboyantes, allant du jaune au rouge intense, offrent un spectacle époustouflant. Parfait pour des randonnées et des promenades, ce lieu unique en France vous plonge dans des paysages dignes de l'Ouest américain. Idéal pour les amoureux de la nature et de la photographie, le Colorado Provençal est une escapade incontournable pour profiter de la beauté et de la diversité.\n",
+        image: "/LECOLORADOPROVENÇAL.jpg",
+        image2: "/4image.svg",
+        price: 6,
+        repas: "sur place",
+        nbSalleDeBain: 2,
+        wifi: "gratuit",
+        nbPersonne: 5,
+        category: "Sport",
+        avis: "Nathan W : \"Visite impressionnante des Catacombes de Paris. L'endroit est fascinant et un peu effrayant, parfait pour les amateurs d'histoire. Je recommande vivement !\"\n",
+        avis2: "Myrielle M. : \"Super expérience ! Les Catacombes de Paris sont vraiment uniques. On apprend beaucoup sur l'histoire de la ville. À ne pas manquer si vous êtes à Paris.\"\n"
+
+    },
+    {
+        id: 11,
+        name: "Mongolfière",
+        position: "Londres",
+        content:
+            "Découvrez l'Angleterre et ses richesses culturelles et gastronomiques. Profitez d'un voyage de rêve en Angleterre.",
+        image: "/Mongolfière_1.jpg",
+        image2: "/4image.svg",
+        price: 3500,
+        repas: "sur place",
+        nbSalleDeBain: 1,
+        wifi: "gratuit",
+        nbPersonne: 6,
+        category: "Culture",
+        avis: "Nathan W : \"Visite impressionnante des Catacombes de Paris. L'endroit est fascinant et un peu effrayant, parfait pour les amateurs d'histoire. Je recommande vivement !\"\n",
+        avis2: "Myrielle M. : \"Super expérience ! Les Catacombes de Paris sont vraiment uniques. On apprend beaucoup sur l'histoire de la ville. À ne pas manquer si vous êtes à Paris.\"\n"
+    },
+    {
+        id: 12,
+        name: "Safari",
+        position: "Cote d'ivoire",
+        content:
+            "Découvrez l'Amérique et ses richesses culturelles et gastronomiques. Profitez d'un voyage de rêve en Amérique.",
+        image: "/Mongolfière.jpg",
+        image2: "/4image.svg",
+        price: 2500,
+        repas: "sur place",
+        nbSalleDeBain: 2,
+        wifi: "gratuit",
+        nbPersonne: 7,
+        category: "Sport",
+        avis: "Nathan W : \"Visite impressionnante des Catacombes de Paris. L'endroit est fascinant et un peu effrayant, parfait pour les amateurs d'histoire. Je recommande vivement !\"\n",
+        avis2: "Myrielle M. : \"Super expérience ! Les Catacombes de Paris sont vraiment uniques. On apprend beaucoup sur l'histoire de la ville. À ne pas manquer si vous êtes à Paris.\"\n"
+    },
+];
+
+
+
+export default Activity
+
--- mflix/pages/api/auth/[...nextauth].js
+++ mflix/pages/api/auth/[...nextauth].js
@@ -15,6 +15,7 @@ const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
+
         }),
         CredentialsProvider({
             name: "credentials",
